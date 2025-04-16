46484a48b5b51c476ffc8c532784ea8827de89e4
AryanP-656 <aryanphadnis656@gmail.com>
2025-04-16 15:16:52 +0530

    Task Assignment with deadlines and priorities

--- supabase/migrations/20250414065755_init_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tasks (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    title text NOT NULL,
-    description text,
-    status text DEFAULT 'pending',
-    created_at timestamptz DEFAULT now()
-);
